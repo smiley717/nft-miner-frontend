78deaf2d79d8a3d26735ffb0639ebd69839e76fd
smiley717 <smiley.dev717@gmail.com>
2022-03-01 14:51:59 -0800

    Add video in the homepage, show wallet connect message if not connected

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,15 +4,16 @@ export default function HomePage() {
   return (
     <Row className="align-center" id="home">
       <div className="h-100 position-relative p-0">
-        <img
+        {/* <img
           src="images/mining-pool.jpg"
           className="img-fluid shadow-4 w-100"
-        />
-        <div className="position-absolute w-100 h-100 d-flex start-0 top-0 justify-center align-center">
+        /> */}
+        <video autoPlay muted loop src="video/intro.mp4" className="w-100" />
+        {/* <div className="position-absolute w-100 h-100 d-flex start-0 top-0 justify-center align-center">
           <p className="text-light mb-0 fs-1 fw-bold">
             The World's first ever mining service on Web3
           </p>
-        </div>
+        </div> */}
       </div>
     </Row>
   );
--- src/components/Mint.js
+++ src/components/Mint.js
@@ -2,12 +2,17 @@ import { useEffect, useState } from "react";
 import { Row, Col, Button, DropdownButton, Dropdown } from "react-bootstrap";
 import toast from "react-hot-toast";
 import { ethers } from "ethers";
+import { useWeb3React } from "@web3-react/core";
 import ContractAbi from "../Abis/kor-nft-abi.json";
 require("dotenv").config();
 
 export default function Mint() {
   const [totalMinerTypes, setTotalMinerTypes] = useState(0);
   const [miners, setMiners] = useState([]);
+
+  const { chainId, active } = useWeb3React();
+  const validNetwork =
+    chainId === parseInt(process.env.REACT_APP_CHAIN_ID) ? true : false;
 
   const provider = new ethers.providers.Web3Provider(window.ethereum);
   const KorMintContract = new ethers.Contract(
@@ -17,28 +22,30 @@ export default function Mint() {
   );
 
   useEffect(async () => {
-    const _totalMinerTypes = await KorMintContract.totalMinerTypes();
-    setTotalMinerTypes(_totalMinerTypes.toNumber());
-    console.log(_totalMinerTypes.toNumber());
+    if (validNetwork && active) {
+      const _totalMinerTypes = await KorMintContract.totalMinerTypes();
+      setTotalMinerTypes(_totalMinerTypes.toNumber());
+      console.log(_totalMinerTypes.toNumber());
 
-    let newArr = [];
-    for (let i = 0; i < _totalMinerTypes; i++) {
-      const miner = await KorMintContract.miners(i);
-      const minted = await KorMintContract.mintedMinerCount(
-        miner.hashrate.toNumber()
-      );
-      const total = miner.numOfMiner.toNumber() / 4;
-      const available = total - minted.toNumber() / 4;
+      let newArr = [];
+      for (let i = 0; i < _totalMinerTypes; i++) {
+        const miner = await KorMintContract.miners(i);
+        const minted = await KorMintContract.mintedMinerCount(
+          miner.hashrate.toNumber()
+        );
+        const total = miner.numOfMiner.toNumber() / 4;
+        const available = total - minted.toNumber() / 4;
 
-      newArr.push({
-        hashrate: miner.hashrate.toNumber(),
-        total: total,
-        available: available,
-        price: (miner.price / 10 ** 18).toString(),
-      });
+        newArr.push({
+          hashrate: miner.hashrate.toNumber(),
+          total: total,
+          available: available,
+          price: (miner.price / 10 ** 18).toString(),
+        });
+      }
+      setMiners(newArr);
     }
-    setMiners(newArr);
-  }, []);
+  }, [validNetwork, active]);
 
   const handleMint = async (index, num) => {
     const hashrate = miners[index].hashrate;
@@ -79,51 +86,70 @@ export default function Mint() {
           Mint a NFT, Get Kadena Miner
         </p>
       </div>
-      <Row className="miner-rows">
-        {Array.from({ length: parseInt(miners.length) }, (_, i) => 0 + i).map(
-          (index) => {
-            return (
-              <Col key={index}>
-                <div className="d-flex justify-center">
-                  <img
-                    src={`images/miners/${miners[index].hashrate}ths.jpg`}
-                    className="img-fluid miner-img"
-                  />
-                </div>
-                <div className="d-flex justify-center text-light mt-3 mb-0 fs-5">
-                  Price: {miners[index].price} ETH
-                </div>
-                <div className="d-flex justify-center text-light mb-0 fs-5">
-                  Total: {miners[index].total}
-                </div>
-                <div className="d-flex justify-center text-light mb-0 fs-5">
-                  Available: {miners[index].available}
-                </div>
-                <div className="d-flex justify-center mintBtn">
-                  <DropdownButton
-                    id="dropdown-basic-button"
-                    title="Mint"
-                    size="lg"
-                  >
-                    <Dropdown.Item onClick={() => handleMint(index, 1)}>
-                      Mint 1/4
-                    </Dropdown.Item>
-                    <Dropdown.Item onClick={() => handleMint(index, 2)}>
-                      Mint 2/4
-                    </Dropdown.Item>
-                    <Dropdown.Item onClick={() => handleMint(index, 3)}>
-                      Mint 3/4
-                    </Dropdown.Item>
-                    <Dropdown.Item onClick={() => handleMint(index, 4)}>
-                      Mint 4/4
-                    </Dropdown.Item>
-                  </DropdownButton>
-                </div>
-              </Col>
-            );
-          }
-        )}
-      </Row>
+      {!active ? (
+        <Row>
+          <div className="text-center fs-2 fw-bold text-body">
+            Please connect your wallet
+          </div>
+        </Row>
+      ) : !validNetwork ? (
+        <Row>
+          <div className="text-center fs-2 fw-bold text-body">
+            Please switch your network into Ethereum main network
+          </div>
+        </Row>
+      ) : (
+        <Row className="miner-rows">
+          {Array.from({ length: parseInt(miners.length) }, (_, i) => 0 + i).map(
+            (index) => {
+              return (
+                <Col key={index} className="d-flex justify-center">
+                  <div className="miner-box">
+                    <div className="d-flex justify-center">
+                      <img
+                        src={`images/miners/${miners[index].hashrate}ths.jpg`}
+                        className="img-fluid miner-img"
+                      />
+                    </div>
+                    <div className="d-flex justify-center text-light mt-3 mb-0 fs-5">
+                      <span className="font-1">Price: </span>&nbsp; &nbsp;
+                      <span className="font-2">{miners[index].price} ETH</span>
+                    </div>
+                    <div className="d-flex justify-center text-light mb-0 fs-5">
+                      <span className="font-1">Total: </span>&nbsp; &nbsp;
+                      <span className="font-2">{miners[index].total}</span>
+                    </div>
+                    <div className="d-flex justify-center text-light mb-0 fs-5">
+                      <span className="font-1">Available: </span>&nbsp; &nbsp;
+                      <span className="font-2">{miners[index].available}</span>
+                    </div>
+                    <div className="d-flex justify-center mintBtn">
+                      <DropdownButton
+                        id="dropdown-basic-button"
+                        title="Mint"
+                        size="lg"
+                      >
+                        <Dropdown.Item onClick={() => handleMint(index, 1)}>
+                          Mint 1/4
+                        </Dropdown.Item>
+                        <Dropdown.Item onClick={() => handleMint(index, 2)}>
+                          Mint 2/4
+                        </Dropdown.Item>
+                        <Dropdown.Item onClick={() => handleMint(index, 3)}>
+                          Mint 3/4
+                        </Dropdown.Item>
+                        <Dropdown.Item onClick={() => handleMint(index, 4)}>
+                          Mint 4/4
+                        </Dropdown.Item>
+                      </DropdownButton>
+                    </div>
+                  </div>
+                </Col>
+              );
+            }
+          )}
+        </Row>
+      )}
     </Row>
   );
 }
